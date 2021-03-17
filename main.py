8af7efea0e56946ae64e5e8979d0eb6cc071146b
Colin Sandman <xx56sandmanxx@gmail.com>
2021-03-16 21:40:58 -0400

    creating balance system

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import discord
 import os
 from discord.ext import commands
+import json
+import random
+import os
 #from keep_alive import keep_alive
 
 intents = discord.Intents().all()
@@ -39,9 +42,58 @@ async def on_member_join(member):
     role = discord.utils.get(member.guild.roles, name="Sandling")
     await member.add_roles(role)
 
-#with open('token.txt') as f:
-#    TOKEN = f.readline()
+with open('token.txt') as f:
+    TOKEN = f.readline()
+
+@client.command()
+async def bal(ctx):
+    await open_account(ctx.author)
+    user = ctx.author
+    users = await getBankData()
+
+    wallet_amt = users[str(user.id)]["wallet"]
+    bank_amt = users[str(user.id)]["bank"]
+
+    em =discord.Embed(title=f"{ctx.author.name}'s balance",color=discord.Color.blue())
+    em.add_field(name="Wallet",value=wallet_amt)
+    em.add_field(name="Bank",value=bank_amt)
+    await ctx.send(embed=em)
+
+@client.command()
+async def beg(ctx):
+    await open_account(ctx.author)
+    user = ctx.author
+    users = await getBankData()
+
+    earnings = random.randrange(101)
+    await ctx.send(f"Someone gave you {earnings} coins!")
+
+    users[str(user.id)]["wallet"] += earnings
+
+    with open("bank.json","w") as f:
+        json.dump(users,f)
+
+
+async def open_account(user):
+    users = await getBankData()
+
+    if str(user.id) in users:
+        return False
+    else:
+        users[str(user.id)]={}
+        users[str(user.id)]["wallet"] = 0
+        users[str(user.id)]["bank"] = 0
+
+    with open("bank.json","w") as f:
+        json.dump(users,f)
+    return True
+
+async def getBankData():
+    with open("bank.json","r") as f:
+        users = json.load(f)
+
+    return users
 
 token = os.environ.get('TOKEN')
 #keep_alive()
-client.run(token)
+client.run(TOKEN)

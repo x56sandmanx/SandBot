a38afac9a08fc7b515a6d9ca6ee3cb3a53ec77b1
Colin Sandman <xx56sandmanxx@gmail.com>
2021-06-01 13:04:13 -0400

    moving auto roels to cogs TESTING

---
+++ cogs/reactions.py
@@ -0,0 +1,63 @@
+import discord
+from discord.ext import commands
+
+class Reaction(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        message_id = payload.message_id
+        if message_id == 845362897592778813:
+            guild_id = payload.guild_id
+            guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
+
+            if payload.emoji.name == 'nsfw':
+                role = discord.utils.get(guild.roles, name='NSFW')
+            elif payload.emoji.name =='boy':
+                role = discord.utils.get(guild.roles, name='13-16')
+            elif payload.emoji.name=='teen':
+                role = discord.utils.get(guild.roles, name='17-19')
+            elif payload.emoji.name=='man':
+                role = discord.utils.get(guild.roles, name='20+')
+            elif payload.emoji.name=='male_sign':
+                role = discord.utils.get(guild.roles, name='Male')
+            elif payload.emoji.name=='female_sign':
+                role = discord.utils.get(guild.roles, name='Female')
+            else:
+                role = discord.utils.get(guild.roles, name=payload.emoji.name)
+
+            if role is not None:
+                member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
+                if member is not None:
+                    await member.add_roles(role)
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload):
+        message_id = payload.message_id
+        if message_id == 845362897592778813:
+            guild_id = payload.guild_id
+            guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
+
+            if payload.emoji.name == 'nsfw':
+                role = discord.utils.get(guild.roles, name='NSFW')
+            elif payload.emoji.name =='boy':
+                role = discord.utils.get(guild.roles, name='13-16')
+            elif payload.emoji.name=='teen':
+                role = discord.utils.get(guild.roles, name='17-19')
+            elif payload.emoji.name=='man':
+                role = discord.utils.get(guild.roles, name='20+')
+            elif payload.emoji.name=='male_sign':
+                role = discord.utils.get(guild.roles, name='Male')
+            elif payload.emoji.name=='female_sign':
+                role = discord.utils.get(guild.roles, name='Female')
+            else:
+                role = discord.utils.get(guild.roles, name=payload.emoji.name)
+
+            if role is not None:
+                member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
+                if member is not None:
+                    await member.remove_roles(role)
+    
+def setup(client):
+    client.add_cog(Reaction(client))
